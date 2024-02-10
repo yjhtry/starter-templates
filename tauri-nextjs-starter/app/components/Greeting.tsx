97b6cf4949b301ddf5abf93b3f582fc9c1e7aabc
yjh <860622588@qq.com>
2024-02-10 20:45:39 +0800

    chore: update deps

--- tauri-nextjs-starter/app/components/Greeting.tsx
+++ tauri-nextjs-starter/app/components/Greeting.tsx
@@ -1,19 +1,17 @@
-
 'use client'
 
-import { invoke } from '@tauri-apps/api';
-import { sendNotification } from '@tauri-apps/api/notification';
+import { invoke } from '@tauri-apps/api'
+import { sendNotification } from '@tauri-apps/api/notification'
 
 export default function Greeting() {
   const onClick = async () => {
-    const response: string = await invoke('hello', { msg: 'tauri' });
+    const response: string = await invoke('hello', { msg: 'tauri' })
 
-    console.log(response);
-    sendNotification(response);
+    sendNotification(response)
   }
   return (
     <div>
-      <button onClick={onClick} className='bg-gray-50 rounded-md px-4 py-2 text-black'>
+      <button onClick={onClick} className="bg-gray-50 rounded-md px-4 py-2 text-black">
         hello
       </button>
     </div>
