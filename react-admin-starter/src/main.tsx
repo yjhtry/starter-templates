bef431222671847915979e24f4c90ca62b6fbb3f
yjh <860622588@qq.com>
2024-04-23 17:11:00 +0800

    fix: antd tailwind confict

--- react-admin-starter/src/main.tsx
+++ react-admin-starter/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import './index.css'
 
 // Set up a Router instance
 const router = createRouter({
--- react-admin-starter/src/routes/__root.tsx
+++ react-admin-starter/src/routes/__root.tsx
@@ -1,13 +1,14 @@
 import { createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { StyleProvider } from '@ant-design/cssinjs'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <StyleProvider hashPriority="high">
       <ProgressBar />
       <Outlet />
-      <TanStackRouterDevtools />
-    </>
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
+    </StyleProvider>
   ),
   notFoundComponent: () => {
     return <p>Not Found (on root route)</p>
